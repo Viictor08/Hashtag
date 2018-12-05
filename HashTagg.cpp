225645bbe57342fdf1fbaf71b024bcdaa2dd5737
João Victor <joaovictornap@hotmail.com>
2018-12-05 17:53:41 -0300

    HashTag

--- HashTagg.cpp
+++ HashTagg.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-
 void pvp();
 int EscolhaJogador();
 void tabuleiro1(char matriz[3][3]);
@@ -9,14 +8,11 @@ void tabuleiro2(char matrizes[5][5]);
 void logica1(int entrada);
 void logica2(int entrada);
 void BemVindo();
-
-
 int main (){
 	setlocale(LC_ALL,"portuguese");
 	BemVindo();
 	system("pause");
 	system("cls");
-
 	int Escolha;
 		do{
 			printf("\nBem vindo! Favor escolher sua forma de jogo abaixo\nDigite 1 -> pvp\nDigite 2 -> Sair\n");
@@ -35,7 +31,6 @@ int main (){
 		}while(Escolha != 3);
 	return 0;
 }
-
 void pvp(){
 	int EscolherTamanho;
 	char matriz[3][3] = {{' ', ' ', ' '},
@@ -69,7 +64,6 @@ void tabuleiro1(char matriz[3][3]){
 	printf("\t-----------\n");
 	printf("\t %c | %c | %c \n", matriz[2][0], matriz[2][1], matriz[2][2]);
 }
-
 void tabuleiro2(char matrizes[5][5]){
 	system("cls");
 	printf("\n         Jogo da velha 5x5\n\n");
@@ -83,7 +77,6 @@ void tabuleiro2(char matrizes[5][5]){
 	printf("\t-------------------\n");
 	printf("\t %c | %c | %c | %c | %c \n", matrizes[4][0], matrizes[4][1], matrizes[4][2], matrizes[4][3], matrizes[4][4]);
 }
-
 int EscolhaJogador(){
 	char aux, jogador1, jogador2;
 	printf("Jogador 1 será X ou O ? [Digite 1 para X, Digite 2 para O]:\n");
@@ -99,13 +92,11 @@ int EscolhaJogador(){
 		return 1;
 	}
 }
-
 void logica1(int entrada){
-
+	
 	char matriz[3][3] = {{'1', '2', '3'},
 						{'4', '5', '6'},
 						{'7', '8', '9'}};
-
 	char RespostaUsuario,aux;
   	int ContadorJogadas, linha, coluna, vez = entrada, i, j, venc1 = 0, venc2 = 0, empate = 0, cpu;
   	do{
@@ -129,13 +120,16 @@ void logica1(int entrada){
 			scanf("%i", &coluna);
 			if(linha < 1 || linha > 3 || coluna < 1 || coluna > 3){
 				printf("Opção inválida. Tente novamente!\n");
-			}else if(matriz[linha-1][coluna-1] != ' '){
+			}
+			else if(matriz[linha-1][coluna-1] != ' '){
 				linha = 0;
 				coluna = 0;
-			}else{
+			}
+			else{
 				if(vez %2 == 0){
 					matriz[linha-1][coluna-1] = 'X';
-				}else{
+				}
+				else{
 					matriz[linha-1][coluna-1] = 'O';
 				}
 				vez++;
@@ -185,13 +179,11 @@ void logica1(int entrada){
 	  scanf("%s", &RespostaUsuario);
   	}while(RespostaUsuario=='s');
 }
-
 void logica2(int entrada){
 
 	char matrizes[5][5] = {{' ', ' ', ' ', ' ', ' '},
 						 {' ', ' ', ' ', ' ', ' '},
 						 {' ', ' ', ' ', ' ', ' '}};
-
 	char RespostaUsuario,aux;
   	int ContadorJogadas, linha, coluna, vez = entrada, i, j, venc1 = 0, venc2 = 0, empate = 0;
   	do{
@@ -215,13 +207,16 @@ void logica2(int entrada){
 			scanf("%i", &coluna);
 			if(linha < 1 || linha > 5 || coluna < 1 || coluna > 5){
 				printf("Opção inválida. Tente novamente!\n");
-			}else if(matrizes[linha-1][coluna-1] != ' '){
+			}
+			else if(matrizes[linha-1][coluna-1] != ' '){
 				linha = 0;
 				coluna = 0;
-			}else{
+			}
+			else{
 				if(vez %2 == 0){
 					matrizes[linha-1][coluna-1] = 'X';
-				}else{
+				}
+				else{
 					matrizes[linha-1][coluna-1] = 'O';
 				}
 				vez++;
